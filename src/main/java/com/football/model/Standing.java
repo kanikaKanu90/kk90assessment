ebd722d51611ed090c7cb64762c8ba110610e2ed
Kapoor <amtq4p@splm.siemens.com>
2021-03-20 23:25:06 +0530

    assessment commit

--- src/main/java/com/football/model/Standing.java
+++ src/main/java/com/football/model/Standing.java
@@ -1,13 +1,19 @@
 package com.football.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Standing {
 
 	private String country_id;
+	private String country_name;
 	private String league_id;
+	private String league_name;
 	private String team_id;
+	private String team_name;
 	private String overall_league_position;
 
 }
